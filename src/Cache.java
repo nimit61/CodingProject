d5fb0e717c23dbb49e204c7384ad31f20c48a1b3
Nimit Garg <nimit61993@gmail.com>
2016-09-15 00:04:32 +0530

    Made cache as interface and LRUCache as its implementation

--- src/Cache.java
+++ src/Cache.java
@@ -1,42 +1,6 @@
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class Cache {
+public interface Cache {
 	
-	CacheList cacheList;
-	Map<Integer, CacheNode> cacheMap;
-	int capacity;
-	
-	public Cache(int capacity) {
-		this.cacheList = new CacheList();
-		this.cacheMap = new HashMap<Integer, CacheNode>();
-		this.capacity = capacity;
-	}
-	
-	public int get(int key) {
-		if(!cacheMap.containsKey(key)) {
-			return -1;
-		}
-		CacheNode cachedNode = cacheMap.get(key);
-		CacheNode newNode = new CacheNode(cachedNode.getKey(), cachedNode.getValue());
-		cacheList.insertAtBeginning(newNode);
-		cacheList.deleteLastNode();
-		return newNode.getValue();
-	}
-	
-	public void set(int key, int value) {
-		if(cacheMap.containsKey(key)) {
-			return;
-		}
-		CacheNode node = new CacheNode(key, value);
-		if(cacheList.getNodeCount() == capacity) {
-			cacheMap.remove(cacheList.getLastNode().key);
-			cacheList.deleteLastNode();
-		} 
-		cacheList.insertAtBeginning(node);
-		cacheMap.put(key, node);
-	}
+	public int get(int key);
+	public void set(int key, int value);
 
 }
---
+++ src/LRUCacheTester.java
@@ -0,0 +1,16 @@
+public class LRUCacheTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cache cache = new LRUCache(2);
+		cache.set(1, 10);
+		cache.set(5, 12);
+		System.out.println(cache.get(5));
+		System.out.println(cache.get(1));
+		System.out.println(cache.get(10));
+		cache.set(6, 14);
+		System.out.println(cache.get(5));
+	}
+
+}
+
